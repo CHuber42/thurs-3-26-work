3ee33650d3df1545ce4aa6b211f60892b0d69b97
Christopher Huber <CHuber42.work@gmail.com>
2020-03-25 15:14:58 -0700

    add fixed js for appointments, add cooking conversions: lbs-to-oz, oz-to-cups, cups-to-grams

--- js/scripts-wed-etc.js
+++ js/scripts-wed-etc.js
@@ -2,12 +2,30 @@ $(document).ready(function() {
   $("form#shouting-form").submit(function(event) {
     event.preventDefault();
     $("#shout-output").text($("#shouting-entry").val().toUpperCase());
+  });
     
   $("form#appointments").submit(function(event) {
     event.preventDefault();
     $("#appointments-output").show();
-    
-    });
-
   });
+
+  $("form#lbs-to-oz").submit(function(event) {
+    event.preventDefault();
+    $("#lbs-to-oz-results").text(parseInt($("#lbs-to-oz-request").val()) * 16);
+  });
+
+  $("form#oz-to-cups").submit(function(event) {
+    event.preventDefault();
+    $("#oz-to-cups-results").text(parseInt($("#oz-to-cups-request").val()) / 8);
+  });
+
+  $("form#cups-to-grams").submit(function(event) {
+    event.preventDefault();
+    $("#cups-to-grams-results").text(parseInt($("#cups-to-grams-request").val()) * 340);
+  });
+
+    
+  
+
+  
 });
