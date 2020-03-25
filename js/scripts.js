9f4f0a06ccee3031fd7efe6195b41d1461c2f9e1
Christopher Huber <CHuber42.work@gmail.com>
2020-03-25 10:32:54 -0700

    add letter background, position of opener/signature

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     $(".your-name").text($("input#person-name").val());
   
-
     $("#letter").show();
 
     event.preventDefault();
