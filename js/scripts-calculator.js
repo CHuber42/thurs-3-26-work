b7fd0c9588a7dc29366eaacb88da8c9039cede31
Christopher Huber <CHuber42.work@gmail.com>
2020-03-25 11:55:12 -0700

    added/implemented forms for subtract multiply and divide

--- js/scripts-calculator.js
+++ js/scripts-calculator.js
@@ -17,12 +17,37 @@
 
 
 $(document).ready(function(){
+  
   $("form#add").submit(function(event) {
   event.preventDefault();
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
   var result = add(number1, number2);
-  $("#output").text(result);
+  $("#output1").text(result);
+  }); 
+  
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#output2").text(result);
   });
+  
+    $("form#multiply").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#mul1").val());
+      var number2 = parseInt($("#mul2").val());
+      var result = multiply(number1, number2);
+      $("#output3").text(result);
+    });
+    
+    $("form#divide").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#div1").val());
+      var number2 = parseInt($("#div2").val());
+      var result = divide(number1, number2);
+      $("#output4").text(result);
+     });
 });
 
