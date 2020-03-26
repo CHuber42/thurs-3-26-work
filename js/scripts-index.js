6ddda39bbffd7bbad077b108c15b15b2e3dc10d6
Christopher Huber <CHuber42.work@gmail.com>
2020-03-26 08:52:11 -0700

    initial

--- js/scripts-index.js
+++ js/scripts-index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
  
+
+  $("form#voter-form").submit(function(event) {
+    if (parseInt($("user-age").val()) >= 18 ){
+
+    }
+    else {
+      
+    }
+
+  });
 });
