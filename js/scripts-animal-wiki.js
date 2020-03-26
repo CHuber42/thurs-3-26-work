b60e15b8414d517027dfe60325d09c69546ac34e
Christopher Huber <CHuber42.work@gmail.com>
2020-03-26 10:22:58 -0700

    Add if/else selections to animalChange function

---
+++ js/scripts-animal-wiki.js
@@ -0,0 +1,23 @@
+function animalChange () {
+  var e = $("#animals").val();    
+  alert(e);
+  if (e === "Snake"){
+    $("#snakeSelect").show();
+  }
+  else if (e === "Turtle"){
+    $("#turtleSelect").show();
+  }
+  else if (e === "Insect"){
+    $("#insectSelect").show();
+    }
+  }
+};
+
+
+
+$(document).ready(function(){
+ 
+
+  
+});
+
